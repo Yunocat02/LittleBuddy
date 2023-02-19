9c55c1e7fce125d747be83e9f7417c4cb9170b7e
Yunocat02 <p50248.48@gmail.com>
2023-02-19 21:44:44 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/littlebuddy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.littlebuddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register/Login"),
+      ),
+    );
+  }
+}
