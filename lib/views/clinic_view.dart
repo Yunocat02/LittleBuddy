afad292aa6a7d76299238f67792341f0fb0d54af
Yunocat02 <p50248.48@gmail.com>
2023-03-06 02:23:41 +0700

    add bottomnavigator for clinic and pets

--- lib/views/clinic_view.dart
+++ lib/views/clinic_view.dart
@@ -1,5 +1,12 @@
+import 'package:LittleBuddy/views/Chatbot_view.dart';
 import 'package:LittleBuddy/views/cam_view.dart';
+import 'package:LittleBuddy/views/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../utils/styles.dart';
+import 'datareportviewsmember.dart';
+import 'mypets_view.dart';
 
 class Clinic extends StatefulWidget {
   const Clinic({Key? key}) : super(key: key);
@@ -8,11 +15,37 @@ class Clinic extends StatefulWidget {
   State<Clinic> createState() => _ClinicState();
 }
 
+List navItems = [
+  {
+    'text': 'Adopt',
+    'icon': 'assets/nav_icons/pill_icon.svg',
+    'page': const Home()
+  },
+  {
+    'text': 'Clinix',
+    'icon': 'assets/nav_icons/heart_icon.svg',
+    'page': const Clinic()
+  },
+  {
+    'text': 'Pets',
+    'icon': 'assets/nav_icons/vet_icon.svg',
+    'page': const Mypets()
+  },
+  {
+    'text': 'Help',
+    'icon': 'assets/nav_icons/help_icon.svg',
+    'page': datareportviewsmember()
+  },
+];
+
 class _ClinicState extends State<Clinic> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ยินดีต้อนรับ สู่หน้า clinic")),
+      appBar: AppBar(
+        title: Text("ยินดีต้อนรับ สู่หน้า clinic"),
+        automaticallyImplyLeading: false,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0), // add some padding
         child: Column(
@@ -38,7 +71,12 @@ class _ClinicState extends State<Clinic> {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Chatbot()),
+                  );
+                },
                 child: Text('Chatbot', style: TextStyle(fontSize: 24)),
               ),
             ),
@@ -73,6 +111,49 @@ class _ClinicState extends State<Clinic> {
               ),
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 85,
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+            color: Styles.bgColor),
+        padding: const EdgeInsets.fromLTRB(25, 20, 25, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: navItems.map<Widget>((e) {
+            return InkWell(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(e['icon'],
+                      height: 20,
+                      color: navItems.indexOf(e) == 1
+                          ? Styles.highlightColor
+                          : null),
+                  Text(
+                    e['text'],
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: navItems.indexOf(e) == 1
+                            ? Styles.highlightColor
+                            : Styles.blackColor,
+                        fontWeight:
+                            navItems.indexOf(e) == 1 ? FontWeight.bold : null),
+                  )
+                ],
+              ),
+              onTap: () async {
+                if (navItems.indexOf(e) == 0 ||
+                    navItems.indexOf(e) == 1 ||
+                    navItems.indexOf(e) == 2 ||
+                    navItems.indexOf(e) == 3) {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => e['page']));
+                }
+              },
+            );
+          }).toList(),
         ),
       ),
     );
--- lib/views/mypets_view.dart
+++ lib/views/mypets_view.dart
@@ -2,7 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+
+import '../utils/styles.dart';
+import 'clinic_view.dart';
+import 'datareportviewsmember.dart';
+import 'home.dart';
 
 class Mypets extends StatefulWidget {
   const Mypets({super.key});
@@ -10,6 +16,7 @@ class Mypets extends StatefulWidget {
   @override
   State<Mypets> createState() => _Mypets();
 }
+
 class _Mypets extends State<Mypets> {
   final Future<FirebaseApp> firebase = Firebase.initializeApp();
   final firestore = FirebaseFirestore.instance;
@@ -17,6 +24,29 @@ class _Mypets extends State<Mypets> {
   late FirebaseAuth _auth;
   final _user = Rxn<User>();
   late Stream<User?> _authStateChanges;
+
+  List navItems = [
+    {
+      'text': 'Adopt',
+      'icon': 'assets/nav_icons/pill_icon.svg',
+      'page': const Home()
+    },
+    {
+      'text': 'Clinix',
+      'icon': 'assets/nav_icons/heart_icon.svg',
+      'page': const Clinic()
+    },
+    {
+      'text': 'Pets',
+      'icon': 'assets/nav_icons/vet_icon.svg',
+      'page': const Mypets()
+    },
+    {
+      'text': 'Help',
+      'icon': 'assets/nav_icons/help_icon.svg',
+      'page': datareportviewsmember()
+    },
+  ];
 
   @override
   void initState() {
@@ -39,10 +69,10 @@ class _Mypets extends State<Mypets> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mypet'),
+        automaticallyImplyLeading: false,
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: firestore
@@ -55,14 +85,15 @@ class _Mypets extends State<Mypets> {
             return ListView.builder(
               itemCount: subCollectionSnapshot.data!.docs.length,
               itemBuilder: (context, index) {
-                final data = subCollectionSnapshot.data!.docs[index].data() as Map<String, dynamic>;
+                final data = subCollectionSnapshot.data!.docs[index].data()
+                    as Map<String, dynamic>;
                 return ListTile(
                   title: Text(data['name'] ?? "N/A"),
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(data['type'] ?? "N/A"),
-                      Text(data['age(year)'] ?? "N/A"),   
+                      Text(data['age(year)'] ?? "N/A"),
                       Text(data['age(month)'] ?? "N/A"),
                       Text(data['species'] ?? "N/A"),
                     ],
@@ -74,6 +105,49 @@ class _Mypets extends State<Mypets> {
             return const Center(child: CircularProgressIndicator());
           }
         },
+      ),
+      bottomNavigationBar: Container(
+        height: 85,
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(25)),
+            color: Styles.bgColor),
+        padding: const EdgeInsets.fromLTRB(25, 20, 25, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: navItems.map<Widget>((e) {
+            return InkWell(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(e['icon'],
+                      height: 20,
+                      color: navItems.indexOf(e) == 3
+                          ? Styles.highlightColor
+                          : null),
+                  Text(
+                    e['text'],
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: navItems.indexOf(e) == 3
+                            ? Styles.highlightColor
+                            : Styles.blackColor,
+                        fontWeight:
+                            navItems.indexOf(e) == 3 ? FontWeight.bold : null),
+                  )
+                ],
+              ),
+              onTap: () async {
+                if (navItems.indexOf(e) == 0 ||
+                    navItems.indexOf(e) == 1 ||
+                    navItems.indexOf(e) == 2 ||
+                    navItems.indexOf(e) == 3) {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => e['page']));
+                }
+              },
+            );
+          }).toList(),
+        ),
       ),
     );
   }
