1beac9320c9ed29f9eef92d71efbfe05889bbdb9
Niruthub <s6303051623055@email.kmutnb.ac.th>
2023-03-07 01:15:02 +0700

    map_view ส่วนด้านล่างกับ bar ด้านบน

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapnaja extends StatefulWidget {
@@ -12,7 +13,20 @@ class _MapnajaState extends State<Mapnaja> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("ค้นหา Clinic")),
+      appBar: AppBar(
+        title: Text("ค้นหา Clinic"),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.home),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
       body: const MapScreen(),
     );
   }
@@ -52,9 +66,16 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     // สร้างรายการ Marker ของร้านครินิคไว้
-    List<Widget> DataMarker = [];
-    DataMarker.add(Text("ร้านที่ 1"));
-    DataMarker.add(Text("ร้านที่ 2"));
+    // List<Widget> DataMarker = [];
+    // // DataMarker.add(Text("ร้านที่ 1"));
+    // // DataMarker.add(Text("ร้านที่ 2"));
+    // // วนลูปสร้าง Marker
+    // for (var i = 0; i < 5; i++) {
+    //   DataMarker.add(Text(
+    //     "ร้านที่ ${i + 1}",
+    //   ));
+    // }
+
     return Scaffold(
       // หน้าต่างสำเร็จรูป จัดแอพ
       body: Column(
@@ -84,7 +105,36 @@ class _MapScreenState extends State<MapScreen> {
                 color: Color.fromARGB(255, 167, 196, 219),
                 borderRadius: BorderRadius.circular(20)),
             height: 250,
-            child: ListView(), // ทำเป็นรายการ
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                    itemCount: 5, // จำนวนร้านที่แสดง
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Container(
+                          // กรอบของแต่ละรายการ
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color.fromARGB(255, 118, 133, 145),
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          // เนื้อใน
+                          child: ListTile(
+                              title: Text(
+                                "ร้านที่ ${index + 1}",
+                                style: TextStyle(fontSize: 25),
+                              ),
+                              subtitle: Text("ร้านนี้ดีนะ"),
+                              // ในส่วนของการเลือกร้าน
+                              onTap: () {
+                                print("คุณเลือกร้านที่ ${index + 1}");
+                              }),
+                        ),
+                      );
+                    })),
           ),
         ],
       ),
@@ -94,4 +144,27 @@ class _MapScreenState extends State<MapScreen> {
               CameraUpdate.newCameraPosition(_initialCameraPosition))),
     );
   }
+}
+
+// เตรียมข้อมูล list
+List<Widget> getData(int count) {
+  List<Widget> DataMarker = [];
+  for (var i = 0; i < count; i++) {
+    // ตัวแปร txt ไว้เพิ่มเข้าไปใน list
+    var txt = Text(
+      "ร้านที่ ${i + 1}",
+      style: TextStyle(fontSize: 25),
+    );
+    // ตัวแปร txt2 ไว้เพิ่มเข้าไปใน list แบบ listtile
+    var txt2 = ListTile(
+      title: Text(
+        "ร้านที่ ${i + 1}",
+        style: TextStyle(fontSize: 25),
+      ),
+      subtitle: Text("ร้านนี้ดีนะ"),
+    );
+    // เพิ่มเข้าไปใน list
+    DataMarker.add(txt2);
+  }
+  return DataMarker;
 }
